7d8e2efe961e8ce7ad31237a3095dffd72bbbe78
Zander Meyer Gous <zander_gous@Zanders-Mac-Studio.local>
2025-12-11 11:34:25 +0200

    Changed comment to see if the webhook will trigger

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -16,4 +16,4 @@ export default function Instruments() {
   );
 }
 
-// This comment is just to check the impact on the coolify insfrastructure
+// This comment is just to check the impact on the coolify insfrastructure #2
