10df77d7f01bbcc4497cd2b7bc1cf22c71891c74
Zander Meyer Gous <zander_gous@Zanders-Mac-Studio.local>
2025-12-18 13:47:45 +0200

    Fresh start main

--- supabase/migrations/20251217153709_added_rating_column.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."instruments" add column "rating" smallint;
-
-
--- supabase/migrations/20251218082546_add_product_stock.sql
+++
@@ -1,3 +0,0 @@
-alter table public.products 
-add column stock_quantity integer default 0 not null,
-add column in_stock boolean generated always as (stock_quantity > 0) stored;
